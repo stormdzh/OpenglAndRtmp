22c50053d3dd000b1c2e299034598f7785502170
dzh <411788973@qq.com>
2020-05-01 09:21:57 +0800

    first commit

---
+++ app/src/main/java/com/stormdzh/openglandrtmp/MainActivity.java
@@ -0,0 +1,74 @@
+package com.stormdzh.openglandrtmp;
+
+import android.opengl.GLES20;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private SurfaceView surfaceView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        initSurfaceView();
+    }
+
+    private int count=0;
+    private void initSurfaceView() {
+        surfaceView = findViewById(R.id.surfaceView);
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+
+            }
+
+            @Override
+            public void surfaceChanged(final SurfaceHolder holder, int format, final int width, final int height) {
+
+                new Thread() {
+                    @Override
+                    public void run() {
+                        super.run();
+                        EglHelper eglHelper = new EglHelper();
+                        eglHelper.initEgl(holder.getSurface(), null);
+
+                        while (true) {
+                            GLES20.glViewport(0, 0, width, height);
+
+                            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+                            if(count%2==0) {
+                                GLES20.glClearColor(1.0f, 1.0f, 0.0f, 1.0f);
+                            }else{
+                                GLES20.glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+                            }
+                            count++;
+                            eglHelper.swapBuffers();
+
+                            try {
+                                Thread.sleep(1600);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }.start();
+
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                //egl.destroy
+            }
+        });
+    }
+}

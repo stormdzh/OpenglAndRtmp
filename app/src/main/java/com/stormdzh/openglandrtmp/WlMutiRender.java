835f86d8f45fe03417e59bba73102c485fda3294
dzh <411788973@qq.com>
2020-05-02 16:22:16 +0800

    图片上面在绘制三角形

--- app/src/main/java/com/stormdzh/openglandrtmp/WlMutiRender.java
+++ app/src/main/java/com/stormdzh/openglandrtmp/WlMutiRender.java
@@ -1,7 +1,10 @@
 package com.stormdzh.openglandrtmp;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.opengl.GLES20;
+import android.opengl.GLUtils;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -20,7 +23,13 @@ public class WlMutiRender implements WLEGLSurfaceView.WlGLRender {
             -1f, -1f,
             1f, -1f,
             -1f, 1f,
-            1f, 1f
+            1f, 1f,
+
+            //三角形
+            -0.25f,-0.25f,
+            0.25f,-0.25f,
+            0f,0.25f,
+
     };
     private FloatBuffer vertexBuffer;
 
@@ -43,6 +52,8 @@ public class WlMutiRender implements WLEGLSurfaceView.WlGLRender {
     private int textureId ;
 
     private int index;
+
+    private int imgTextureId;
 
     public void setTextureId(int textureId,int index) {
         this.textureId=textureId;
@@ -95,6 +106,9 @@ public class WlMutiRender implements WLEGLSurfaceView.WlGLRender {
         GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, 0);
 
 
+        imgTextureId=loadTexrute(R.drawable.girl);
+
+
     }
 
     @Override
@@ -124,7 +138,45 @@ public class WlMutiRender implements WLEGLSurfaceView.WlGLRender {
 
         GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
 
+
+        drawFrameTwo();
+
         GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
         GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, 0);
+    }
+
+    private int loadTexrute(int src) {
+        int[] textureIds = new int[1];
+        GLES20.glGenTextures(1, textureIds, 0);
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureIds[0]);
+
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_REPEAT);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_REPEAT);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), src);
+        GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0);
+
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
+        return textureIds[0];
+
+    }
+
+
+    private void drawFrameTwo() {
+
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, imgTextureId);
+
+        GLES20.glEnableVertexAttribArray(vPosition);
+        GLES20.glVertexAttribPointer(vPosition, 2, GLES20.GL_FLOAT, false, 8,
+                32);
+
+        GLES20.glEnableVertexAttribArray(fPosition);
+        GLES20.glVertexAttribPointer(fPosition, 2, GLES20.GL_FLOAT, false, 8,
+                vertexData.length * 4);
+//        GLES20.glViewport(0, 0, width, height);
+
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 3);
     }
 }

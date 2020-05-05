54021ae201d4e772a8514b8796c3aeffe66b3e17
dzh <411788973@qq.com>
2020-05-05 23:33:31 +0800

    opengl渲染yuv数据

--- app/src/main/java/com/stormdzh/openglandrtmp/MainActivity.java
+++ app/src/main/java/com/stormdzh/openglandrtmp/MainActivity.java
@@ -57,6 +57,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, ImageVideoActivity.class));
             }
         });
+        findViewById(R.id.showYUV).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, YuvActivity.class));
+            }
+        });
 
     }
 
